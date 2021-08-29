d9315728eed4f99ef5ca65746636eaeb7da5b087
BeomsooKim <kbsat@naver.com>
2021-08-29 17:05:57 +0900

    카테고리 테이블 추가

---
+++ src/main/java/com/daom/domain/Category.java
@@ -0,0 +1,20 @@
+package com.daom.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
