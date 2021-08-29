ab6be865aa7dad7c76c6608f274457eab15b7eb4
BeomsooKim <kbsat@naver.com>
2021-08-29 16:55:06 +0900

    UnivRepository 생성

---
+++ src/main/java/com/daom/repository/UnivRepository.java
@@ -0,0 +1,11 @@
+package com.daom.repository;
+
+import com.daom.domain.Member;
+import com.daom.domain.Univ;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UnivRepository extends JpaRepository<Univ, Long> {
+    Optional<Univ> findByName(String name);
+}
